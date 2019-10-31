0e0da6c561e7f31227966e230a7bc56aea71162c
xiaocongkaifa <36947295+xiaocongkaifa@users.noreply.github.com>
2019-10-31 15:47:13 +0800

    粘包测试

---
+++ swoole_study/03/pack/client.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 小丛
+ * Date: 29.10.2019
+ * Time: 21:59
+ */
+$client = new swoole\Client(SWOOLE_SOCK_TCP);
+
+$client->connect("127.0.0.1", 9800);
+
+// 约定一个分隔符
+// 一次性发多条数据
+for($i = 0;$i<= 10;$i++){
+    $client->send("123456\r\n");
+}
+// 一次发送大量数据,拆分成小数据
+$body = json_encode(str_repeat('A',12*1024*1024));
+// 数据包头+包体
+$data = pack('N',strlen($body)).$body;
+$client->send($data);
+echo $client->recv(); //接收消息没有收到
