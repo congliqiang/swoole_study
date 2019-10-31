a79720e671fc85167d69889a7732fdd8c44b8cbd
xiaocongkaifa <36947295+xiaocongkaifa@users.noreply.github.com>
2019-10-31 14:37:01 +0800

    粘包测试

--- swoole_study/02/pack/client.php
+++ swoole_study/02/pack/client.php
@@ -8,7 +8,12 @@
 $client = new swoole\Client(SWOOLE_SOCK_TCP);
 
 $client->connect("127.0.0.1", 9800);
-for($i = 0;$i<= 10;$i++){
-    $client->send("123456");
-}
+// 一次性发多条数据
+//for($i = 0;$i<= 10;$i++){
+//    $client->send("123456");
+//}
+
+// 一次发送大量数据,拆分成小数据
+$data = str_repeat('A',12*1024*1024);
+$client->send(json_encode($data));
 //echo $client->recv(); //接收消息没有收到
