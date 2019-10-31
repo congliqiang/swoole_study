bab365466c553439618d255b552e65fe53c31424
xiaocongkaifa <36947295+xiaocongkaifa@users.noreply.github.com>
2019-10-31 13:50:58 +0800

    粘包测试

--- swoole_study/02/pack/client.php
+++ swoole_study/02/pack/client.php
@@ -9,6 +9,6 @@ $client = new swoole\Client(SWOOLE_SOCK_TCP);
 
 $client->connect("127.0.0.1", 9800);
 for($i = 0;$i<= 10;$i++){
-    $client->sendto("123456");
+    $client->send("123456");
 }
 $client->recv(); //接收消息没有收到
