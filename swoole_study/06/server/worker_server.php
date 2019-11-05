4abaa492049a5489fae5b123cf20b30141aafaa2
congliqiang <congliqiang>
2019-11-05 22:28:57 +0800

    基于epoll的reactor模型的多进程非阻塞模型

--- swoole_study/06/server/worker_server.php
+++ swoole_study/06/server/worker_server.php
@@ -24,26 +24,64 @@ class Worker{
     public $onMessage = NULL;
     public $workerNum = 2;
     public $allSocket; // 存放所有的socket
+    public $addr; //监听的地址
+    public $reusePort = 1;
     public function __construct($socket_address) {
-        // 监听地址+端口
-        $this->socket = stream_socket_server($socket_address);
+        $this->addr = $socket_address;
     }
     // fork 进程
     public function fork(){
-        $this->accept();
+        for ($i=0;$i<=$this->workerNum;$i++){
+            $pid = pcntl_fork(); //创建成功返回子进程id
+            if ($pid < 0){
+                exit('创建失败');
+            }else if($pid > 0) {
+                //父进程空间,返回子进程id
+            }else{
+                // 子进程空间
+                $this->accept();
+            }
+        }
+        $status = 0;
+        $pid = pcntl_wait($status); // 会返回结束的子进程信息,阻塞状态
+        echo "子进程回收了:".$pid.PHP_EOL;
     }
 
     public function start() {
         // 获取配置文件
         // 监听进程
-        $this->fork();  // 用来创建多个子进程,负责接收请求的
+        for ($i=0;$i<=$this->workerNum;$i++){
+            $pid = pcntl_fork(); //创建成功返回子进程id
+            if ($pid < 0){
+                exit('创建失败');
+            }else if($pid > 0) {
+                //父进程空间,返回子进程id
+                $a = 2;
+                $status = 0;
+                echo "子进程回收了".$pid.PHP_EOL;
+            }else{
+                // 子进程空间
+                $this->accept();
+            }
+        }
+        $pid = pcntl_wait($status); // 会返回结束的子进程信息,阻塞状态
     }
 
     public function accept(){
+        $opts = array(
+            'socket' => array(
+                'backlog' => 10240, // 成功建立socket连接的等待个数
+            ),
+        );
+        $context = stream_context_create($opts);
+        // 开启多端口的监听,并且实现负载均衡
+        stream_context_set_option($context,'socket','so_reuseport',1);
+
+        $this->socket = stream_socket_server($this->addr,$errno,$errstr,STREAM_SERVER_BIND|STREAM_SERVER_LISTEN,$context);
         // 第一个需要监听的事件(服务端socket的事件),一旦监听到可读事件之后会触发
         swoole_event_add($this->socket,function ($fd){
-           $clientSocket = stream_socket_accept($fd);
-           // 监听客户端可读事件
+            $clientSocket = stream_socket_accept($fd);
+            // 监听客户端可读事件
             swoole_event_add($clientSocket,function ($fd){
                 // 连接中读取客户端的数据
                 $buffer = fread($fd,65535);
@@ -67,6 +105,9 @@ class Worker{
 
 
 $worker = new Worker('tcp://0.0.0.0:9800');
+
+// 开启多进程的端口监听
+$worker->reusePort = true;
 
 $worker->onConnect = function ($fd) {
     echo '新的连接来了'.(int)$fd.PHP_EOL;
